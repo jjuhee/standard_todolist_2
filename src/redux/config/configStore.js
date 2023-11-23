7f0e8ba2417dca2ac48275d3345c5930ebc9897f
wonee09 <wonee09@gmail.com>
2023-11-23 10:53:37 +0900

    기초자료 세팅

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,8 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
-import todos from "../modules/todos";
 
-// store를 만들어 내기
-// 중앙데이터관리소
 const rootReducer = combineReducers({
-  todos: todos,
+  //
 });
 
 const store = createStore(rootReducer);
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -4,27 +4,9 @@ import shortid from "shortid";
 const initialState = [
   {
     id: shortid.generate(),
-    title: "리액트1",
-    body: "리액트 공부하기1",
+    title: "",
+    body: "",
     isDone: false,
-  },
-  {
-    id: shortid.generate(),
-    title: "리액트2",
-    body: "리액트 공부하기2",
-    isDone: true,
-  },
-  {
-    id: shortid.generate(),
-    title: "리액트3",
-    body: "리액트 공부하기3",
-    isDone: false,
-  },
-  {
-    id: shortid.generate(),
-    title: "리액트4",
-    body: "리액트 공부하기4",
-    isDone: true,
   },
 ];
 
@@ -32,19 +14,13 @@ const initialState = [
 const todos = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return [...state, action.payload];
+      return; //TODO: 여기 작성
 
     case "DELETE_TODO":
-      return state.filter((todo) => todo.id !== action.payload);
+      return; //TODO: 여기 작성
 
     case "SWITCH_TODO":
-      return state.map((todo) => {
-        if (todo.id === action.payload) {
-          return { ...todo, isDone: !todo.isDone };
-        } else {
-          return todo;
-        }
-      });
+      return; //TODO: 여기 작성
 
     default:
       return state;
